c03c94bbb78a0b88d1bb63ea19d107ab7165aa2f
rashigarg25 <rashi.garg25@gmail.com>
2024-09-07 15:23:07 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,68 +1,76 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from PIL import Image
+import requests
 import torch
 from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
 from qwen_vl_utils import process_vision_info
 import time
 
+class ImageRequest(BaseModel):
+    imagePath: str
+    query: str
+
 
 start = time.process_time()
 
+
+app = FastAPI()
+
 device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
 
-#device = "cpu"
-# default: Load the model on the available device(s)
 model = Qwen2VLForConditionalGeneration.from_pretrained(
    "Qwen/Qwen2-VL-7B-Instruct", torch_dtype="auto", device_map="auto"
 ).to(device)
 
-# model = Qwen2VLForConditionalGeneration.from_pretrained(
-#      "Qwen/Qwen2-VL-7B-Instruct",
-#      torch_dtype=torch.bfloat16,
-#      attn_implementation="flash_attention_2",
-#      device_map="auto",
-# )
 
-
-# default processer
 processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct")
 
+@app.post("/processImage")
+async def process_image(request: ImageRequest):
+    
+    image_path = request.imagePath
+    query = request.query
 
-messages = [
-    {
-        "role": "user",
-        "content": [
-            {
-                "type": "image",
-                "image": "../wa/18.jpeg",
-            },
-            {
-                "type": "text", 
-                "text": "What is the value of uhid of each image? Only the image name with value, don't share text"},
-        ],
-    }
-]
-
-
-text = processor.apply_chat_template(
-    messages, tokenize=False, add_generation_prompt=True)
-
-image_inputs, video_inputs = process_vision_info(messages)
-inputs = processor(
-    text=[text],
-    images=image_inputs,
-    videos=video_inputs,
-    padding=True,
-    return_tensors="pt"
-).to(device)
+    image = Image.open(image_path)
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                },
+                {
+                    "type": "text", 
+                    "text": query,
+                }    
+            ],
+        }
+    ]
 
 
-# Inference: Generation of the output
-generated_ids = model.generate(**inputs, max_new_tokens=512)
-generated_ids_trimmed = [
-    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-]
-output_text = processor.batch_decode(
-    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
-)
-print(output_text)
+    text = processor.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True)
 
-print(time.process_time() - start)
+    #image_inputs, video_inputs = process_vision_info(messages)
+    inputs = processor(
+        text=[text],
+        images=[image],
+        padding=True,
+        return_tensors="pt"
+    ).to(device)
+
+
+    # Inference: Generation of the output
+    generated_ids = model.generate(**inputs, max_new_tokens=512)
+    generated_ids_trimmed = [
+        out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+    ]
+    output_text = processor.batch_decode(
+        generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+    )
+    print(output_text)
+    print(time.process_time() - start)
+
+    return output_text
+
